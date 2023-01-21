73822e31cfa0b20c2220d4ca4db66127cb0bb3ab
Katya <K_at@mail.ru>
2023-01-21 15:36:27 +0800

    Первый коммит

---
+++ src/main/java/com/example/kursovay3/Kursovay3Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovay3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovay3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovay3Application.class, args);
+    }
+
+}
